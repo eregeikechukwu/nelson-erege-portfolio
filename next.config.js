675f3a2a5185f91fb4dc688343dbc4809bf74efb
eregeikechukwu <eregeikechukwu@gmail.com>
2025-03-08 11:10:05 +0100

    disabled static expoer in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: true, // Enables React's strict mode to help catch potential issues
   swcMinify: true,
-  output: "export",
 };
 
 module.exports = nextConfig;
